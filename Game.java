8e11877ae14d075d9e116a3fdf9aac56015d78b8
alex0l0tl <leighalexander980@gmail.com>
2020-04-21 11:49:46 +0100

    First version with functional JFrame

---
+++ Game.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Image;
+public class Game
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Program is running, you just fucked up.");
+        Table pool= new Table();
+    }
+}
---
+++ Table.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Table
+{
+	private JPanel poolTable=new JPanel();
+	private JFrame window = new JFrame();
+	private ImageIcon table=new ImageIcon("Table.png");
+	private JLabel label = new JLabel(table);
+	public Table()
+	{
+		this.window=new JFrame();
+		this.poolTable=new JPanel();
+		poolTable.add(label);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(750,750);
+        window.setVisible(true);
+	}
+	public static void main(String[] args)
+    {
+        System.out.println("Program Running");
+    }
+}
