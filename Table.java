e1106344e7a67b732397eb509737387637e5a6bb
alex0l0tl <leighalexander980@gmail.com>
2020-04-21 16:27:29 +0100

    Pool ball sucessfully added

--- Table.java
+++ Table.java
@@ -6,12 +6,16 @@ public class Table
 	private JPanel poolTable=new JPanel();
 	private JFrame window = new JFrame();
 	private ImageIcon table=new ImageIcon("Table.png");
-	private JLabel label = new JLabel(table);
+	private ImageIcon ball1=new ImageIcon("1ball.png");//Balls are set at 38x38 pixels and should probably be decalred in an array
+	private JLabel lblTable = new JLabel(table);
+	private JLabel lblBall1 = new JLabel(ball1);
 	public Table()
 	{
 		this.window=new JFrame();
-		this.poolTable=new JPanel();
-		poolTable.add(label);
+		this.poolTable=new JPanel();//Need to work out how to overlap different labels
+		//poolTable.add(lblTable);
+		poolTable.add(lblBall1);
+		poolTable.add(lblTable);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(980,600);
         window.setVisible(true);
