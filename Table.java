1613d6899818ea083884506a139d79264d97640e
alex0l0tl <leighalexander980@gmail.com>
2020-04-22 10:15:44 +0100

    New assets added and declared in array

--- Table.java
+++ Table.java
@@ -6,15 +6,15 @@ public class Table
 	private JPanel poolTable=new JPanel();
 	private JFrame window = new JFrame();
 	private ImageIcon table=new ImageIcon("Table.png");
-	private ImageIcon ball1=new ImageIcon("1ball.png");//Balls are set at 38x38 pixels and should probably be decalred in an array
+	private ImageIcon[] balls={new ImageIcon("1ball.png"),new ImageIcon("2ball.png"),new ImageIcon("3ball.png"),new ImageIcon("4ball.png"),new ImageIcon("5ball.png"),new ImageIcon("6ball.png"),new ImageIcon("7ball.png"),new ImageIcon("8ball.png"),new ImageIcon("WhiteBall.png")};
 	private JLabel lblTable = new JLabel(table);
-	private JLabel lblBall1 = new JLabel(ball1);
+	//private JLabel lblBall1 = new JLabel(ball1);
 	public Table()
 	{
 		this.window=new JFrame();
 		this.poolTable=new JPanel();//Need to work out how to overlap different labels
 		//poolTable.add(lblTable);
-		poolTable.add(lblBall1);
+		//poolTable.add(lblBall1);
 		poolTable.add(lblTable);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(980,600);
