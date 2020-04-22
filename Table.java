a02fb4c85fe13dfc1c698e12c755d8c18b542991
alex0l0tl <leighalexander980@gmail.com>
2020-04-22 15:39:27 +0100

    LayeredPane added

--- Table.java
+++ Table.java
@@ -5,6 +5,7 @@ public class Table
 {
 	private JPanel poolTable=new JPanel();
 	private JFrame window = new JFrame();
+	private JLayeredPane layeredPane= new JLayeredPane();
 	private ImageIcon table=new ImageIcon("Table.png");
 	private ImageIcon poolCue=new ImageIcon("PoolCue.png");
 	private ImageIcon[] balls={new ImageIcon("1ball.png"),new ImageIcon("2ball.png"),new ImageIcon("3ball.png"),new ImageIcon("4ball.png"),new ImageIcon("5ball.png"),new ImageIcon("6ball.png"),new ImageIcon("7ball.png"),new ImageIcon("8ball.png"),new ImageIcon("WhiteBall.png")};
@@ -13,17 +14,24 @@ public class Table
 	public Table()
 	{
 		this.window=new JFrame();
-		this.poolTable=new JPanel();//Need to work out how to overlap different labels
-		//poolTable.add(lblTable);
+		
 		//poolTable.add(lblBall1);
-		poolTable.add(lblTable);
+		//poolTable.add(lblTable);
+		this.layeredPane= new JLayeredPane();
+		layeredPane.setPreferredSize(new Dimension(980, 590));
+		layeredPane.setBorder(BorderFactory.createTitledBorder(
+                                    "Test message"));
+		layeredPane.add(lblTable);
+		//this.poolTable=new JPanel();
+		//poolTable.add(lblTable);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(980,600);
+        window.setSize(2000,1000);
         window.setVisible(true);
-		window.setContentPane(poolTable);
+		window.setContentPane(layeredPane);
+
 	}
 	public static void main(String[] args)
     {
-        System.out.println("Program Running");
+
     }
 }
